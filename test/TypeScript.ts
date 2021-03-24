cb5ef4f2a8c7adab368893c45d5ddd36b0ecdd66
gckalex <alexolland@getcodingknowledge.com>
2021-03-24 21:26:58 +0100

    Fixed Typescript support

--- test/TypeScript.ts
+++ test/TypeScript.ts
@@ -1,131 +1,124 @@
-import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo';
-import { Message, ColorResolvable } from 'discord.js';
-import mongoose from "mongoose";
-import { GuildsProvider, PlayersProvider } from '../structures/providers';
-import { Logger } from '../util/Logger';
-import { join } from 'path';
+import { Players } from '../models/Players';
+import items from '../game/data/items.json';
 
-declare module 'discord-akairo' {
-  interface AkairoClient {
-    commandHandler: CommandHandler,
-    listenerHandler: ListenerHandler,
-    config: AkairoBotOptions,
-    colorConstants: ColorConstants,
-    idConstants: IdConstants,
-    guildSettings: GuildsProvider,
-    player: PlayersProvider,
-    logger: typeof Logger,
-  }
-}
+export default class Mechanics {
+  public player: Players;
+  public monster: Monsters;
 
-interface AkairoBotOptions {
-  owner?: string,
-  token?: string
-}
-
-interface IdConstants {
-  mainModLog: string
-}
-
-interface ColorConstants {
-  errorColor: ColorResolvable,
-  warnColor: ColorResolvable,
-  succeedColor: ColorResolvable,
-  infoColor: ColorResolvable,
-  otherColor: ColorResolvable
-}
-
-export default class AkairoBotClient extends AkairoClient {
-  public guildSettings: GuildsProvider;
-
-  public commandHandler: CommandHandler = new CommandHandler(this, {
-    directory: join(__dirname, '..', 'commands'),
-    prefix: async(message: Message): Promise<string> => {
-      const _ = await this.guildSettings.get(message.guild!);
-      if (_) return _.prefix;
-      // @ts-ignore
-      return process.env.prefix;
-    },
-    allowMention: true,
-    handleEdits: true,
-    commandUtil: true,
-    commandUtilLifetime: 3e5,
-    defaultCooldown: 3e3,
-    argumentDefaults: {
-      prompt: {
-        modifyStart: (_: any, str: string): string => `${str}\nType \`cancel\` to cancel the command.`,
-        modifyRetry: (_: any, str: string): string => `${str}\nType \`cancel\` to cancel the command.`,
-        timeout: 'Error: Command timed out.',
-        ended: 'Error: Too many attempts.',
-        cancel: 'Command has been cancelled.',
-        retries: 3,
-        time: 3e4
-      },
-      otherwise: '',
-    }
-  });
-
-  public listenerHandler: ListenerHandler = new ListenerHandler(this, {
-    directory: join(__dirname, '..', 'listeners')
-  });
-
-  public player : PlayersProvider;
-  public config: AkairoBotOptions;
-  public logger: typeof Logger;
-  public colorConstants: ColorConstants;
-  public idConstants : IdConstants;
-
-  public constructor(config: AkairoBotOptions) {
-    super({ ownerID: config.owner }, {
-      fetchAllMembers: true,
-      messageCacheMaxSize: 10e3,
-      messageCacheLifetime: 3600,
-    });
-
-    this.config = config;
-    this.logger = Logger;
-    this.guildSettings = new GuildsProvider();
-    this.player = new PlayersProvider();
-    this.colorConstants = {
-      errorColor: 'RED',
-      warnColor: 'ORANGE',
-      succeedColor: 'GREEN',
-      infoColor: 'NAVY',
-      otherColor: 'PURPLE'
-    };
-    this.idConstants = {
-      mainModLog: '820374615427383347'
-    };
+  public constructor(player: Players, monster: Monsters) {
+    this.player = player;
+    this.monster = monster;
   }
 
-  private async _init(): Promise<void> {
-    this.commandHandler.useListenerHandler(this.listenerHandler);
-    this.listenerHandler.setEmitters({
-      commandHandler: this.commandHandler,
-      listenerHandler: this.listenerHandler
-    });
-
-    this.commandHandler.loadAll();
-    this.logger.log(`Commands -> ${this.commandHandler.modules.size}`);
-    this.listenerHandler.loadAll();
-    this.logger.log(`Listeners -> ${this.listenerHandler.modules.size}`);
+  private defense(): number {
+    const addedDef = this.calculateAddedDef();
+    return Math.floor(1.5 * this.player.attributs.str + 0.4 * (this.player.attributs.dex + this.player.attributs.int) + addedDef);
   }
 
-  public async start(): Promise<string> {
-    try {
-      await mongoose.connect(process.env.mongostring as string, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-      }); this.logger.log("Mongoose connected!");
-    } catch(e) {
-      this.logger.error('DB not connected!');
-      this.logger.error(e);
-      return process.exit();
+  private calculateAddedDef(): number {
+    const helm = items.find(e => e.name == this.player.gear.helm);
+    const chest = items.find(e => e.name == this.player.gear.chest);
+    const legs = items.find(e => e.name == this.player.gear.legs);
+    const boots = items.find(e => e.name == this.player.gear.boots);
+    const addedDef = helm?.armor! + chest?.armor! + legs?.armor! + boots?.armor!;
+    return addedDef;
+  }
+
+  public monsterAttack(): number {
+    const ratios = this.calculateratioOnLevelDifference();
+    const totalPlayerDef = this.defense();
+    const monsterMinAttack = Math.floor(ratios.monsterAttackRatio * (0.85 * this.monster.att - (ratios.playerDefRatio * totalPlayerDef)));
+    const monsterMaxAttack = Math.floor(ratios.monsterAttackRatio * (this.monster.att - (ratios.playerDefRatio * totalPlayerDef)));
+    const monsterAttackRange = this.generateRangeFromArray(monsterMinAttack, monsterMaxAttack);
+    console.log(monsterAttackRange);
+    return monsterAttackRange[Math.floor(Math.random() * monsterAttackRange.length)];
+  }
+
+  private calculateratioOnLevelDifference(): Ratios {
+    let playerDefRatio: number = 0.55;
+    let monsterAttackRatio: number = 0.85;
+    const levelDifference: number = this.player.level - this.monster!.level;
+
+    switch(levelDifference) {
+      case 5: {
+        playerDefRatio= 0.8;
+        monsterAttackRatio = 0.775;
+        break;
+      }
+      case 4: {
+        playerDefRatio = 0.75;
+        monsterAttackRatio = 0.79;
+        break;
+      }
+      case 3: {
+        playerDefRatio = 0.7;
+        monsterAttackRatio = 0.805;
+        break;
+      }
+      case 2: {
+        playerDefRatio = 0.65;
+        monsterAttackRatio = 0.82;
+        break;
+      }
+      case 1: {
+        playerDefRatio = 0.6;
+        monsterAttackRatio = 0.835;
+        break;
+      }
+      case 0: {
+        playerDefRatio = 0.55;
+        monsterAttackRatio = 0.85;
+        break;
+      }
+      case -1: {
+        playerDefRatio = 0.5;
+        monsterAttackRatio = 0.8575;
+        break;
+      }
+      case -2: {
+        playerDefRatio = 0.45;
+        monsterAttackRatio = 0.865;
+        break;
+      }
+      case -3: {
+        playerDefRatio = 0.4;
+        monsterAttackRatio = 0.8725;
+        break;
+      }
+      case -4: {
+        playerDefRatio = 0.35;
+        monsterAttackRatio = 0.88;
+        break;
+      }
+      case -5: {
+        playerDefRatio = 0.3;
+        monsterAttackRatio = 0.9;
+        break;
+      }
+      default: {
+        break;
+      }
     }
 
-    await this._init();
-    return this.login(this.config.token);
+    return { playerDefRatio, monsterAttackRatio };
+  }
+
+  private generateRangeFromArray(start: number, stop: number, step: number = 1): number[] {
+    return Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
   }
 }
 
+export interface Ratios {
+  playerDefRatio: number,
+  monsterAttackRatio: number
+}
+
+export interface Monsters {
+  name: string,
+  hp: number,
+  att: number,
+  pdr: number,
+  mdr: number,
+  description: string,
+  level: number
+}
